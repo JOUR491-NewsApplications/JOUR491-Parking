28aca37f0eda88c85093b2221dc8c872c60a5115
Matt Waite <matt.waite@gmail.com>
2014-04-25 10:33:17 -0500

    Adding some date query-fu to your repo. KAPOW

--- parking/tickets/views.py
+++ parking/tickets/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from tickets.models import Location, Reason, Ticket
 from django.db.models import Count
+from django.db import connection
 
 def home(request):
     locations = Location.objects.annotate(Count('ticket')).order_by('-ticket__count')[:20]
@@ -10,6 +11,10 @@ def home(request):
 def locationdetail(request, slug):
     loc = Location.objects.get(location_slug=slug)
     tickets = Ticket.objects.filter(location=loc).order_by('-date')
-    return render(request, 'locationdetail.html', {'loc': loc, 'tickets': tickets})
+    total = tickets.count()
+    truncate_date = connection.ops.date_trunc_sql('month','date')
+    qs = tickets.extra({'month':truncate_date})
+    months = qs.values('month').order_by('month').annotate(Count('date'))
+    return render(request, 'locationdetail.html', {'loc': loc, 'tickets': tickets, 'months': months, 'total': total,})
 
 
