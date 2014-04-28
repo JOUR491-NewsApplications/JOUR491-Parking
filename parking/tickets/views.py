292d4699e1d768aa57e5f7fff835a806a856d2fc
Matt Waite <matt.waite@gmail.com>
2014-04-28 17:13:47 -0500

    Fixed date formatting issue on month chart ... finally.

--- parking/tickets/views.py
+++ parking/tickets/views.py
@@ -3,6 +3,15 @@ from django.http import HttpResponse
 from tickets.models import Location, Reason, Ticket
 from django.db.models import Count
 from django.db import connection
+import dateutil.parser
+
+'''
+def weekdaycount(locid):
+    cursor = connection.cursor()
+    cursor.execute("SELECT strftime('%w', date) as dayofweek, COUNT(*) as weekdaycount FROM tickets_ticket WHERE location_id=%i GROUP BY dayofweek") % locid
+    row = cursor.fetchone()
+    return row
+'''
 
 def home(request):
     locations = Location.objects.annotate(Count('ticket')).order_by('-ticket__count')[:20]
@@ -15,6 +24,10 @@ def locationdetail(request, slug):
     truncate_date = connection.ops.date_trunc_sql('month','date')
     qs = tickets.extra({'month':truncate_date})
     months = qs.values('month').order_by('month').annotate(Count('date'))
+    # Loop over month values with enumerate so we get an index value.
+    for i, month in enumerate(months):
+        # Modify each month in place. Parse out the date, which should totally sort of work.
+        months[i]['month'] = dateutil.parser.parse(month['month'])
     return render(request, 'locationdetail.html', {'loc': loc, 'tickets': tickets, 'months': months, 'total': total,})
 
 
